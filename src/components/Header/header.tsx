1f0e6e4408ce135677320f83025d1f467c601868
Артем <pakshaartem@gmail.com>
2025-05-20 01:50:42 +0300

    fixes

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -6,6 +6,7 @@ import { Buttons } from "./Buttons/buttons"
 import { Logout } from "./Logout/logout"
 import { useEffect, useState } from "react";
 import { checkAuth } from "@/helpers/api/checkAuth/checkAuth";
+import { getUser } from "@/helpers/api/getUser/getUser";
 
 export const Header = () => {
     const [user, setUser] = useState(null);
@@ -16,12 +17,18 @@ export const Header = () => {
         });
     }, []);
     
+    useEffect(() => {
+        getUser().then(data => {
+            if (data) setUser(data);
+        });
+    }, []);
+    
     return (
         <div>
             <div className="container flex flex-row justify-between items-center py-[10px]">
                 <HeartLogo />
                 <HeaderNav />
-                {user ? <Logout /> : <Buttons />}
+                {user ? <Logout user={user}/> : <Buttons />}
             </div>
         </div>
     )
