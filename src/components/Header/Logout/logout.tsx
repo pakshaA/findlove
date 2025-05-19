ea5d855818ccf3a66532cebc73a3135b901ebd27
Артем <pakshaartem@gmail.com>
2025-05-20 02:38:52 +0300

    fixes

--- src/components/Header/Logout/logout.tsx
+++ src/components/Header/Logout/logout.tsx
@@ -4,11 +4,10 @@ import { CustomButton } from "@/components/ui/button/button"
 import { logoutUser } from "@/helpers/api/logout/logout";
 import { useRouter } from "next/navigation";
 
-// const DEFAULT_AVATAR = '/defaultAvatar.webp'
+const DEFAULT_AVATAR = '/defaultAvatar.webp'
 
 interface user {
     username: string;
-    photo: string;
 }
 
 export const Logout = ({ user }: { user: user }) => {
@@ -26,7 +25,7 @@ export const Logout = ({ user }: { user: user }) => {
     return (
         <div className="flex flex-row gap-[20px] items-center">
             <Link href="/lk" className="flex flex-row gap-[10px] items-center">
-                <Image src={user.photo} alt='Avatar' width={50} height={50} className='rounded-full w-[50px] h-[50px] hover:border-[#E92063] border-[1px] duration-500'/>
+                <Image src={DEFAULT_AVATAR} alt='Avatar' width={50} height={50} className='rounded-full w-[50px] h-[50px] hover:border-[#E92063] border-[1px] duration-500'/>
                 <p className="text-[18px] hover:text-[#E92063] duration-300">{user.username}</p>
             </Link>
             <CustomButton text="Выйти" style="outlined" link="/" onClick={handleLogout} />
