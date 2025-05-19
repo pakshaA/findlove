f58498934b79f5d186a8c60ab0db50f1c911e156
Артем <pakshaartem@gmail.com>
2025-05-19 20:29:06 +0300

    remove cookies set

--- src/helpers/api/login/login.ts
+++ src/helpers/api/login/login.ts
@@ -1,5 +1,4 @@
 'use client'
-import Cookies from "js-cookie"
 
 export const loginUser = async (email: string, password: string) => {
     const response = await fetch('https://server-9n6t.onrender.com/api/login', {
@@ -12,7 +11,6 @@ export const loginUser = async (email: string, password: string) => {
     })
 
     const data = await response.json()
-    Cookies.set('token', data.token)
     if (response.ok) {
         return true
     } else {
