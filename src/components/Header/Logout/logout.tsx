78c121b1bcfddb56654d3966510a261ac1776257
Артем <pakshaartem@gmail.com>
2025-05-20 02:08:20 +0300

    f

--- src/components/Header/Logout/logout.tsx
+++ src/components/Header/Logout/logout.tsx
@@ -26,7 +26,7 @@ export const Logout = ({ user }: { user: user }) => {
     return (
         <div className="flex flex-row gap-[20px] items-center">
             <Link href="/lk" className="flex flex-row gap-[10px] items-center">
-                <Image src={user.photo} alt='Avatar' width={50} height={50} className='rounded-full w-[50px] h-[50px] hover:border-[#E92063] border-[1px] duration-500'/>
+                <Image src={`https://server-9n6t.onrender.com/${user.photo}`} alt='Avatar' width={50} height={50} className='rounded-full w-[50px] h-[50px] hover:border-[#E92063] border-[1px] duration-500'/>
                 <p className="text-[18px] hover:text-[#E92063] duration-300">{user.username}</p>
             </Link>
             <CustomButton text="Выйти" style="outlined" link="/" onClick={handleLogout} />
