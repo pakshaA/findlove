84d20664f66dca6100d05c896152ebe2b0f251ef
Артем <pakshaartem@gmail.com>
2025-05-19 20:26:17 +0300

    fix

--- src/helpers/api/login/login.ts
+++ src/helpers/api/login/login.ts
@@ -1,3 +1,6 @@
+'use client'
+import Cookies from "js-cookie"
+
 export const loginUser = async (email: string, password: string) => {
     const response = await fetch('https://server-9n6t.onrender.com/api/login', {
         method: 'POST',
@@ -9,6 +12,7 @@ export const loginUser = async (email: string, password: string) => {
     })
 
     const data = await response.json()
+    Cookies.set('token', data.token)
     if (response.ok) {
         return true
     } else {
