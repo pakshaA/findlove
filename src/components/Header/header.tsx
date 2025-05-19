da5aa2667da7d100c9166c4eae906778fa5c25e2
Артем <pakshaartem@gmail.com>
2025-05-19 20:44:14 +0300

    fix

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -5,14 +5,14 @@ import { Logout } from "./Logout/logout"
 import Cookies from "js-cookie"
 
 export const Header = () => {
-    const cookies = Cookies.get('token')
-    console.log(cookies)
+    const token = Cookies.get('token')
+    console.log(token)
     return (
         <div>
             <div className="container flex flex-row justify-between items-center py-[10px]">
                 <HeartLogo />
                 <HeaderNav />
-                {cookies ? <Logout /> : <Buttons />}
+                {token ? <Logout /> : <Buttons />}
             </div>
         </div>
     )
