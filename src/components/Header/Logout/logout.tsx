8aadc5ef600a759d042c9ff9a95f9c567e926f6e
Артем <pakshaartem@gmail.com>
2025-05-20 01:49:32 +0300

    fix

--- src/components/Header/Logout/logout.tsx
+++ src/components/Header/Logout/logout.tsx
@@ -4,9 +4,14 @@ import { CustomButton } from "@/components/ui/button/button"
 import { logoutUser } from "@/helpers/api/logout/logout";
 import { useRouter } from "next/navigation";
 
-const DEFAULT_AVATAR = '/defaultAvatar.webp'
+// const DEFAULT_AVATAR = '/defaultAvatar.webp'
 
-export const Logout = () => {
+interface user {
+    name: string;
+    avatar: string;
+}
+
+export const Logout = ({ user }: { user: user }) => {
     const router = useRouter()
 
     const handleLogout = () => {
@@ -21,8 +26,8 @@ export const Logout = () => {
     return (
         <div className="flex flex-row gap-[20px] items-center">
             <Link href="/lk" className="flex flex-row gap-[10px] items-center">
-                <Image src={DEFAULT_AVATAR} alt='Avatar' width={50} height={50} className='rounded-full w-[50px] h-[50px] hover:border-[#E92063] border-[1px] duration-500'/>
-                <p className="text-[18px] hover:text-[#E92063] duration-300">ИМЯ</p>
+                <Image src={user.avatar} alt='Avatar' width={50} height={50} className='rounded-full w-[50px] h-[50px] hover:border-[#E92063] border-[1px] duration-500'/>
+                <p className="text-[18px] hover:text-[#E92063] duration-300">{user.name}</p>
             </Link>
             <CustomButton text="Выйти" style="outlined" link="/" onClick={handleLogout} />
         </div>
---
+++ src/helpers/api/getUser/getUser.ts
@@ -0,0 +1,16 @@
+'use client'
+
+export const getUser = async () => {
+    const response = await fetch('https://server-9n6t.onrender.com/api/getUser', {
+        method: 'GET',
+        credentials: 'include'
+    })
+
+    const data = await response.json()
+    console.log(data)
+    if (response.ok) {
+        return data
+    } else {
+        throw new Error(data.message)
+    }
+}
