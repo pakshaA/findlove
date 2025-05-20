8027838d844c1e973e34447a257e47946bf03405
Артем <pakshaartem@gmail.com>
2025-05-20 12:03:38 +0300

    f

--- src/helpers/api/updateUser/update.ts
+++ src/helpers/api/updateUser/update.ts
@@ -8,7 +8,7 @@ interface User {
 }
 
 export const updateUser = async (user: User) => {
-    const response = await fetch('https://server-9n6t.onrender.com/api/updateUser', {
+    const response = await fetch('https://server-9n6t.onrender.com/api/update', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -18,7 +18,7 @@ export const updateUser = async (user: User) => {
     })
 
     const data = await response.json()
-    
+
     if (response.ok) {
         console.log(data.user, 'Обновление прошло успешно')
         return true
