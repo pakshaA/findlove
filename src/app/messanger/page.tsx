294554a0dd733ef57af02ae405aa969db78f065b
Артем <pakshaartem@gmail.com>
2025-05-20 13:08:15 +0300

    fix

---
+++ src/app/messanger/page.tsx
@@ -0,0 +1,11 @@
+const Messanger = () => {
+    return (
+        <div>
+            <div className="container flex justify-center items-center">  
+                <h1>Сраница находиться в разработке</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Messanger
