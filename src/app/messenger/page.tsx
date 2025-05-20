9d22d87f2070a7559c072d6140f2814dbb5d7e1c
Артем <pakshaartem@gmail.com>
2025-05-20 13:15:31 +0300

    f

--- src/app/messenger/page.tsx
+++ src/app/messenger/page.tsx
@@ -2,10 +2,10 @@ import { Header } from "@/components/Header/header"
 
 const Messanger = () => {
     return (
-        <div>
+        <div className=" bg-[#F6E7FA] h-[100vh]">
             <Header />
-            <div className="container flex justify-center items-center h-[100vh] bg-[#F6E7FA]">  
-                <h1 className="text-[25px] text-[#E92063]">Сраница находиться в разработке</h1>
+            <div className="container flex justify-center items-center h-[100vh]">  
+                <h1 className="text-[40px] font-bold text-[#E92063]">Сраница находиться в разработке</h1>
             </div>
         </div>
     )
