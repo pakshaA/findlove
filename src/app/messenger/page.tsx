5610aa20cd4bb1c4a2c77e2db8efa2d81a581567
Артем <pakshaartem@gmail.com>
2025-05-20 13:13:42 +0300

    f

--- src/app/messenger/page.tsx
+++ src/app/messenger/page.tsx
@@ -4,8 +4,8 @@ const Messanger = () => {
     return (
         <div>
             <Header />
-            <div className="container flex justify-center items-center">  
-                <h1 className="text-[25px]">Сраница находиться в разработке</h1>
+            <div className="container flex justify-center items-center h-[100vh] bg-[#F6E7FA]">  
+                <h1 className="text-[25px] text-[#E92063]">Сраница находиться в разработке</h1>
             </div>
         </div>
     )
